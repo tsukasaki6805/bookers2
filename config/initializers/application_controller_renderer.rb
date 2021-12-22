a0cc58e217c6fbc8f7cc32bed7f7ad019de52db2
tsukasaki6805 <kohei.tsuka46@gmail.com>
2021-12-22 04:26:50 +0000

    finish2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a818cc5325a6c58e50a63fcd0b3bbb418b39ac45fba36acf923c1e1b7a08d9f456fffc5138e7aa52b23d6c0cf653b8bb6c54ce853cd2ec25e87ea65f9cb5c8b7'
